9f3c1c1942fdaa3616fe5c911a61a97a9a9f1a7c
dickersonsteam <37583333+dickersonsteam@users.noreply.github.com>
2018-12-12 12:01:04 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
+# This example program will make a single sound play out
+# of the selected pin.
+#
+# The following are the default parameters for which pin
+# the sound will come out of, sample rate, note pitch/frequency,
+# and note duration in seconds.
+#
+# audio_pin = board.A0
+# sample_rate = 8000
+# note_pitch = 440
+# note_length = 1
+#
+# This example makes use of print messages to help debug
+# errors. As you make changes and then run your code, watch
+# these messages to understand what is happening (or not happening).
+# When you add features, add messages to help you debug your own
+# code. Ultimately, these print messages do slow things down a bit,
+# but they will make you life easier in the long run.
+
 import audioio
 import board
 import array
 import time
 import math
+
+# record current time
+start_time_pgm = time.monotonic()
 
 # for Feather M0 use A0 only
 # for Feather M4 either A0 or A1 work
@@ -25,15 +47,15 @@ note_length = 1
 # This will take more or less time depending on the
 # frequency and sample_rate that you selected.
 print("Generate sound wave.")
-start_time = time.monotonic()
+start_time_gen = time.monotonic()
 length = sample_rate // note_pitch
 
 sine_wave = array.array("h", [0] * length)
 
-for sample in range(length):
-    sine_wave[sample] = int(math.sin(math.pi * 2 * i / 18) * (2 ** 15))
+for i in range(length):
+    sine_wave[i] = int(math.sin(math.pi * 2 * i / 18) * (2 ** 15))
 
-time_to_completion = time.monotonic() - start_time
+time_to_completion = time.monotonic() - start_time_gen
 
 print("Generating the sound wave took " + str(time_to_completion) + " seconds.")
     
@@ -53,4 +75,7 @@ dac.play(sine_wave, loop=True)
 time.sleep(note_length)
 dac.stop()
 
+# Print program execution time
 print("Program completed execution.")
+time_to_completion = time.monotonic() - start_time_pgm
+print("Execution took " + str(time_to_completion) + " seconds.")
