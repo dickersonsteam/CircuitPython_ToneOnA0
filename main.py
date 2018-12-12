daffe9d44097e2a99ccb36ae951f47e36eabaeed
dickersonsteam <37583333+dickersonsteam@users.noreply.github.com>
2018-12-12 11:51:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,53 @@ import array
 import time
 import math
 
-FREQUENCY = 440    # 440 Hz middle 'A'
-SAMPLERATE = 8000  # 8000 samples/second, recommended!
+# for Feather M0 use A0 only
+# for Feather M4 either A0 or A1 work
+audio_pin = board.A0
 
-# Generate one period of sine wav.
-length = SAMPLERATE // FREQUENCY
-sine_wave = array.array("H", [0] * length)
-for i in range(length):
-    sine_wave[i] = int(math.sin(math.pi * 2 * i / 18) * (2 ** 15) + 2 ** 15)
+# Sample rate roughly determines the quality of the sound.
+# A larger number may sound better, but will take
+# more processing time and memory.
+sample_rate = 8000
 
+# Set the pitch for the note you would like to play.
+# http://pages.mtu.edu/~suits/notefreqs.html
+note_pitch = 440
 
-sample = audioio.AudioOut(board.A0, sine_wave)
-sample.frequency = SAMPLERATE
-sample.play(loop=True)  # keep playing the sample over and over
-time.sleep(1)           # until...
-sample.stop()           # we tell the board to stop
+# Set how many seconds the note will last.
+# Try 0.1 - 1.0 or any number.
+note_length = 1
+
+# Now you must "draw" the sound wave in memory.
+# This will take more or less time depending on the
+# frequency and sample_rate that you selected.
+print("Generate sound wave.")
+start_time = time.monotonic()
+length = sample_rate // note_pitch
+
+sine_wave = array.array("h", [0] * length)
+
+for sample in range(length):
+    sine_wave[sample] = int(math.sin(math.pi * 2 * i / 18) * (2 ** 15))
+
+time_to_completion = time.monotonic() - start_time
+
+print("Generating the sound wave took " + str(time_to_completion) + " seconds.")
+    
+# Now initialize you DAC (aka speaker pin)
+print("Initialize DAC.")
+dac = audioio.AudioOut(audio_pin)
+
+# Convert the sine wave you generated earlier into a
+# sample that can be used with the dac.
+print("Converting to RawSample.")
+sine_wave = audioio.RawSample(sine_wave)
+
+# Play the sine wave on the dac and loop it for however
+# many seconds you set with note_length.
+print("Playing sound.")
+dac.play(sine_wave, loop=True)
+time.sleep(note_length)
+dac.stop()
+
+print("Program completed execution.")
